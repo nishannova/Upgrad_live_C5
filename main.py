48d24d8c6af098e4abdf47b8d8a5a56da4727a63
Nishan Ali <nishanali@Nishans-MacBook-Pro.local>
2022-07-30 21:25:07 +0530

    Revert "Adition of garbage code"
    This reverts commit ea0352e1ef64f6d1148eb10d46d253992a252fb5.

--- main.py
+++ main.py
@@ -7,5 +7,3 @@ y = int(input("Enter another integer: "))
 print(f"Summation of {x} and {y} is: {sum_(x, y)}")
 
 print(f"Multiplication of {x} and {y} is: {mult(x, y)}")
-
-print("Addition of garbage code")
