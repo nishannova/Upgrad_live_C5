57dfa48084da863709662b2aa2055322d1cef3c8
Nishan Ali <nishanali@Nishans-MacBook-Pro.local>
2022-07-30 21:15:48 +0530

    Addition of multiplication

---
+++ multiplication.py
@@ -0,0 +1,8 @@
+def mult(x, y):
+    """
+    This method multiplies 2 integers and returns result
+    :params: x: integer
+    :params: y: integer
+    :return: integer: Multiplication of x & y
+    """
+    return x * y
